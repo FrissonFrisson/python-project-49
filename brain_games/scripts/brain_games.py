bc045dde71a2486c168975eecd93c16374b8a082
FrissonFrisson <compton56rus@gmail.com>
2023-06-07 11:04:09 +0300

    setup Flake8 and make lint on MakeFile. fixed linter errors

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -3,7 +3,9 @@ import cli
 
 
 def main():
-	print("Welcome to the Brain Games!")
-	cli.welcome_user()
+    print("Welcome to the Brain Games!")
+    cli.welcome_user()
+
+
 if __name__ == '__main__':
-	main()
+    main()

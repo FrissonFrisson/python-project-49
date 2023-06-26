f584ae0396e773b365aeec43ba894e96b86a8e86
FrissonFrisson <compton56rus@gmail.com>
2023-06-27 00:30:48 +0300

    add games brain_gsd

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/d86f051eab34933b9ec6/maintainability)](https://codeclimate.com/github/FrissonFrisson/python-project-49/maintainability)
 [![asciicast](https://asciinema.org/a/vP91kEaDw2934otMXxs48R6oT.svg)](https://asciinema.org/a/vP91kEaDw2934otMXxs48R6oT)
 [![asciicast](https://asciinema.org/a/axPSLBykhPqHiHJ4kORVG56fx.svg)](https://asciinema.org/a/axPSLBykhPqHiHJ4kORVG56fx)
+[![asciicast](https://asciinema.org/a/IPKu8laVexmFhR5XOVPVeqSai.svg)](https://asciinema.org/a/IPKu8laVexmFhR5XOVPVeqSai)
---
+++ brain_games/games/brain_gcd.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+from ..scripts import game_tools
+from random import random
+
+
+def NOD(num_1, num_2):
+    n1 = num_1
+    n2 = num_2
+    while n1 or n2 != 0:
+        if n1 == 0:
+            return n2
+        if n2 == 0:
+            return n1
+        a = n1 % n2
+        if a == 0:
+            return n2
+        else:
+            n1 = n2
+            n2 = a
+
+
+def main():
+    questions = [[int(random()*100), int(random()*100)] for i in range(0, 3)]
+    correct_answer = list([str(NOD(x, y)) for x, y in questions])
+    questions = [f'{x} {y}' for x, y in questions]
+    name = game_tools.greetings()
+    print('Find the greatest common divisor of given numbers.')
+    if game_tools.check_answer(correct_answer, questions):
+        print(f'Congratulations, {name}!')
+        return
+    print(f"Let's try again, {name}!")
+    return
+
+
+if __name__ == '__main__':
+    main()

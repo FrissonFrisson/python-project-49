2960dd14d3be5b48ce716a6c7ed8a927e3a9ed8b
FrissonFrisson <compton56rus@gmail.com>
2023-07-24 21:27:22 +0300

    fix code "yes" 'yes'

--- brain_games/games/game_tools.py
+++ brain_games/games/game_tools.py
@@ -13,7 +13,7 @@ def check_answer(correct_answer, questions, name):
         print(f'Question: {question}')
         user_ans = prompt.string('Your answer:')
         if user_ans != ans:
-            print(f'"{user_ans}" is wrong answer ;(. Correct answer was "{ans}".')
+            print(f'\'{user_ans}\' is wrong answer ;(. Correct answer was \'{ans}\'.')
             print(f'Let\'s try again, {name}!')
             return
         print('Correct!')

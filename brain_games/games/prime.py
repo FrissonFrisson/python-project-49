59cd2ba33d8a4eee4d7d73bcd8ddfa487e80ffd6
FrissonFrisson <compton56rus@gmail.com>
2023-07-24 21:46:07 +0300

    fiz problem

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -5,3 +5,7 @@ def question_answer():
     questions = ([randrange(1, 100) for i in range(0, 3)])
     correct_answer = (['yes' if i % 2 == 0 else 'no' for i in questions])
     return correct_answer, questions
+
+
+if __name__ == '__main__':
+    main()
